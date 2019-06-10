047cf367bb3f93b7cc0ef2f22f2cc9d935d5731d
alvaro <alvaropeira980@yahoo.com>
2019-06-10 07:33:50 -0400

    added email new media social share.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+//  @ngx-share-button
 import {ShareButtonModule} from '@ngx-share/button';
 // firebase
 import {AngularFireModule} from 'angularfire2';
@@ -13,12 +14,13 @@ import {environment} from '../environments/environment';
 import {DynamicPageComponent} from './components/dynamic-page/dynamic-page.component';
 //  services
 import {DynamicPageService} from './services/dynamic-page.service';
+//  fontAwesome libraries
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {library} from '@fortawesome/fontawesome-svg-core';
 import {fas} from '@fortawesome/free-solid-svg-icons';
 import {fab} from '@fortawesome/free-brands-svg-icons';
 import {HttpClientModule} from '@angular/common/http';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope';
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons/faEnvelope';
 
 library.add(fas, faEnvelope, fab);
 
